e7d409972f9ff97b18fa0218d54e58418651c7f6
Luis Henrique <lh5818181@gmail.com>
2024-10-02 12:11:45 -0300

    configurando o jquery

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn-buscar-cep').addEventListener('click', function() {
         // AJAX - Asynchronous JavaScript and XML
 
@@ -11,5 +11,29 @@ document.addEventListener('DOMContentLoaded', function() {
         xhttp.send();
 
         //https://viacep.com.br/ws/123123123/json
+    })
+})*/
+
+$(document).ready(function() {
+    $('#btn-buscar-cep').click(function() {
+        const cep = $('#cep').val();
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        const botao = $(this);
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('span').removeClass('d-none');
+
+
+
+        $.ajax(endpoint).done(function(resposta) {
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
+
+            $(botao).find('i').removeClass('d-none');
+            $(botao).find('span').addClass('d-none');
+        })
     })
 })
