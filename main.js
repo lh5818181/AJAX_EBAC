ac148ba964acc0bac2a3e8efbc547923c19cd864
Luis Henrique <lh5818181@gmail.com>
2024-10-03 16:18:49 -0300

    ajustentes no jquery

--- main.js
+++ main.js
@@ -34,15 +34,38 @@ $(document).ready(function() {
         //     const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
         //     $('#endereco').val(endereco);
 
-        //     setTimeout(function() {
-        //         $(botao).find('i').removeClass('d-none');
-        //         $(botao).find('span').addClass('d-none');
-        //     }, 3000);
+
 
         // })
     
         fetch(endpoint).then(function(resposta) {
-            console.log(resposta);
+            return resposta.json();
         })
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
+
+        })
+        .catch(function(erro) {
+            alert("Ocorreu um erro ao buscar o endereço, tento novamente mais tarde.")
+        })
+        .finally(function() {
+            setTimeout(function() {
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 1000);
+        })
+    })
+
+    $('#formulario-pedido').submit(function(evento) {
+        evento.preventDefault();
+
+        if ($('#nome').val().length == 0) {
+            throw new Error('Digite o nome');
+        }
     })
 })
