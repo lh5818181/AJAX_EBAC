45f9d8bf053051cdfac73c9670c8513361226207
Luis Henrique <lh5818181@gmail.com>
2024-10-03 11:58:53 -0300

    ajustando main.js

--- main.js
+++ main.js
@@ -26,19 +26,23 @@ $(document).ready(function() {
 
 
 
-        $.ajax(endpoint).done(function(resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-            $('#endereco').val(endereco);
+        // $.ajax(endpoint).done(function(resposta) {
+        //     const logradouro = resposta.logradouro;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        //     $('#endereco').val(endereco);
 
-            setTimeout(function() {
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
-            }, 3000);
+        //     setTimeout(function() {
+        //         $(botao).find('i').removeClass('d-none');
+        //         $(botao).find('span').addClass('d-none');
+        //     }, 3000);
 
+        // })
+    
+        fetch(endpoint).then(function(resposta) {
+            console.log(resposta);
         })
     })
 })
